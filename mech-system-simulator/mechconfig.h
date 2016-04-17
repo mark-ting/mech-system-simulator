a5fe5a1c0423d2a599aed534fe18e02ca41cc200
Mark Ting <mat3049@gmx.com>
2016-04-17 21:57:30 +0800

    Moved MechConfig variable definitions outside header.

--- mech-system-simulator/mechconfig.h
+++ mech-system-simulator/mechconfig.h
@@ -1,6 +1,5 @@
 #pragma once
 
-// TODO: parse these values from game data instead of hard-coding here.
 namespace MechConfig
 {
 	// Heat Sink Capacity and Dissipation Values
@@ -8,34 +7,32 @@ namespace MechConfig
 	// Dissipation in units of heat / sec
 
 	// IS Single Heat Sink Values
-	const double IS_SHS_INTERNAL_CAPACITY = 1.2;
-	const double IS_SHS_EXTERNAL_CAPACITY = 1.2;
-	const double IS_SHS_INTERNAL_DISSIPATION = -0.11;
-	const double IS_SHS_EXTERNAL_DISSIPATION = -0.12;
+	extern const double IS_SHS_INTERNAL_CAPACITY;
+	extern const double IS_SHS_EXTERNAL_CAPACITY;
+	extern const double IS_SHS_INTERNAL_DISSIPATION;
+	extern const double IS_SHS_EXTERNAL_DISSIPATION;
 
 	// IS Double Heat Sink Values
-	const double IS_DHS_INTERNAL_CAPACITY = 2.0;
-	const double IS_DHS_EXTERNAL_CAPACITY = 1.5;
-	const double IS_DHS_INTERNAL_DISSIPATION = -0.2;
-	const double IS_DHS_EXTERNAL_DISSIPATION = -0.14;
+	extern const double IS_DHS_INTERNAL_CAPACITY;
+	extern const double IS_DHS_EXTERNAL_CAPACITY;
+	extern const double IS_DHS_INTERNAL_DISSIPATION;
+	extern const double IS_DHS_EXTERNAL_DISSIPATION;
 
 	// Clan Single Heat Sink Values
-	const double CLAN_SHS_INTERNAL_CAPACITY = 2;
-	const double CLAN_SHS_EXTERNAL_CAPACITY = 1.2;
-	const double CLAN_SHS_INTERNAL_DISSIPATION = -0.11;
-	const double CLAN_SHS_EXTERNAL_DISSIPATION = -0.12;
+	extern const double CLAN_SHS_INTERNAL_CAPACITY;
+	extern const double CLAN_SHS_EXTERNAL_CAPACITY;
+	extern const double CLAN_SHS_INTERNAL_DISSIPATION;
+	extern const double CLAN_SHS_EXTERNAL_DISSIPATION;
 
 	// Clan Double Heat Sink Values
-	const double CLAN_DHS_INTERNAL_CAPACITY = 2;
-	const double CLAN_DHS_EXTERNAL_CAPACITY = 1.1;
-	const double CLAN_DHS_INTERNAL_DISSIPATION = -0.2;
-	const double CLAN_DHS_EXTERNAL_DISSIPATION = -0.15;
-
+	extern const double CLAN_DHS_INTERNAL_CAPACITY;
+	extern const double CLAN_DHS_EXTERNAL_CAPACITY;
+	extern const double CLAN_DHS_INTERNAL_DISSIPATION;
+	extern const double CLAN_DHS_EXTERNAL_DISSIPATION;
 
 	// Value of (non-elite) skill in %
 	// Cool Run affects dissipation
 	// Heat Containment affects capacity
-
-	const double COOL_RUN = 0.075;
-	const double HEAT_CONTAINMENT = 0.10;
+	extern const double COOL_RUN;
+	extern const double HEAT_CONTAINMENT;
 }
